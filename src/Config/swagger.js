969473b82442c9f3978456f644c628aa3b8930b7
Gopendranath Sasmal <baban56799@gmail.com>
2025-04-27 21:22:53 +0530

    Refactor Swagger configuration and enhance API documentation setup

--- src/Config/swagger.js
+++ src/Config/swagger.js
@@ -1,18 +1,20 @@
 const swaggerJSDoc = require('swagger-jsdoc');
 
 const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Youtube API',
-      version: '1.0.0',
-      description: 'A simple API documentation',
-    },
-    servers: [
-      {
-        url: 'http://localhost:3000',
+  swaggerDefinition: {
+      openapi: '3.0.0',
+      info: {
+          title: 'YouTube Backend API',
+          version: '1.0.0',
+          description: 'API documentation for YouTube subscribers',
       },
-    ],
+      servers: [
+          {
+              url: process.env.NODE_ENV === 'production' 
+                  ? 'https://bknd-yt-ab.onrender.com'
+                  : 'http://localhost:3000',
+          },
+      ],
   },
   apis: ['./src/Routes/*.js'],
 };
